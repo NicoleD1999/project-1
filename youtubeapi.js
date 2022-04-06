600160f55c4aa483e0c1c8c8b349b1964b3d5bf8
Rhoward1 <ryanmhoward@gmail.com>
2022-04-06 11:20:00 -0500

    added coments to js file.

--- youtubeapi.js
+++ youtubeapi.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
-
-    var API_KEY = "AIzaSyB58v8FQf2ARu32WeXiQQsb3coxR19L7HM";
-    
-    // var video =''
-
-    
+// youtube API Key
+    var API_KEY = "AIzaSyDDwKCp2rr58dTbORH9RIfuBJ0alJY8PSY";
+   
 
     $("#form").submit(function(event){
         event.preventDefault()
@@ -13,15 +10,14 @@ $(document).ready(function() {
 
         videoSearch(API_KEY, search, 1)
 
-
     })
-
+//retrieving video from API
     function videoSearch(key, search, maxResults){
-
+//clears video 
         $("#videos").empty()
             
-    // https://www.googleapis.com/youtube/v3/search?part=snippet&key=" + key + "&type=video&q=
-        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search + "-trailer",
+
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search + "-trailer" + "-anime",
         function (data) {
             // console.log(data.items[0].id.videoId)
 
@@ -30,10 +26,10 @@ $(document).ready(function() {
                 var video = $("<iframe>");
                 var url = "https://www.youtube.com/embed/"+item.id.videoId;
                 video.attr("src", url)
+                         
                 
-                
-                // var video = $(`<iframe width="420" height="315" src="https://www.youtube.com/embed/${item.id.videoId}" title="YouTube video player" frameborder="0" allow=" allowfullscreen>`)
 console.log(video);
+// append video to videos div
                 $("#videos").append(video)
             })
         })
