1987dde33c7bfd8b166966a4dac7974bb66a04c5
NicoleD1999 <nmd1018@yahoo.com>
2022-04-06 12:43:45 -0500

    added view trailer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,12 @@ searchBtn.addEventListener("click", function(event){
     var captUserSearch = userSearch.value
     document.location.href="./results.html?search=" + captUserSearch
 })
+
+var searchGenre = document.querySelector(".searchGenre")
+var userGenre = document.querySelector("#myList")
+
+searchGenre.addEventListener("click", function(event){
+event.preventDefault();
+var captUserGenre = userGenre.value
+document.location.href = "./resultsgenre.html?search=" + captUserGenre
+})
