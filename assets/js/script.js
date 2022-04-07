e6476e39f85c32652e41dd3e98442d2b31bbddd5
Kellen Andrea <kellen.andrea.416@gmail.com>
2022-04-07 11:12:20 -0500

    merging main

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-var searchBtn = document.querySelector(".searchBtn")
-var userSearch = document.querySelector(".user-search")
-
-searchBtn.addEventListener("click", function(event){
-    event.preventDefault();
-    var captUserSearch = userSearch.value
-    document.location.href="./results.html?search=" + captUserSearch
-})
